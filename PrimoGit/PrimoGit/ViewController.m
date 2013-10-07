ad03ea389c2c90f664f8a0ddf40545f711c7965b
Daniele Ceglia <daniele.ceglia@relifeit.com>
2013-10-07 10:55:23 +0200

    altro da buttare

--- PrimoGit/PrimoGit/ViewController.m
+++ PrimoGit/PrimoGit/ViewController.m
@@ -18,7 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.lbOmar.text = @"ciao da Omar";
+    self.lbOmar.text = @"ciao da Omar???";
 }
 
 - (void)didReceiveMemoryWarning
