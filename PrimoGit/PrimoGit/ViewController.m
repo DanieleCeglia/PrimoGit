1ffd81e0a797ec6778bdb6d61376f808c66dad01
Daniele Ceglia <daniele.ceglia@relifeit.com>
2013-10-08 15:38:27 +0200

    ripristinato test

--- PrimoGit/PrimoGit/ViewController.m
+++ PrimoGit/PrimoGit/ViewController.m
@@ -20,8 +20,8 @@
 	// Do any additional setup after loading the view, typically from a nib.
     self.lbOmar.text = @"ciao da Omar";
     
-    self.lb1.text = @"master va avanti";
-    self.lb2.text = @"master va avanti";
+    self.lb1.text = @"branch va avanti";
+    self.lb2.text = @"branch va avanti";
 }
 
 - (void)didReceiveMemoryWarning
